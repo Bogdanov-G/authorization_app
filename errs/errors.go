a604629749e6c6e91f4ab76cc9ce2b6cb6693f20
Bogdanov-G <gleb_bogdan@mail.ru>
2022-02-11 18:48:53 +0300

    Fixed comments.

--- errs/errors.go
+++ errs/errors.go
@@ -1,4 +1,4 @@
-// errs package provides:
+// Package errs package provides:
 // - struct for handling error's HTTP code and verbose message
 // as a single object;
 // - methods for the most frequent error’s cases.
@@ -8,42 +8,42 @@ import (
 	"net/http"
 )
 
-// Represents error's message and HTTP status code.
+// AppError Represents error's message and HTTP status code.
 type AppError struct {
 	Code    int    `json:",omitempty"`
 	Message string `json:"message"`
 }
 
-// AsMessage() returns a new error object with omitted HTTP status code.
+// AsMessage returns a new error object with omitted HTTP status code.
 func (e AppError) AsMessage() *AppError {
 	return &AppError{Message: e.Message}
 }
 
-// Creates an error object with HTTP status code 404 and using the given
+// NewNotFoundError Creates an error object with HTTP status code 404, using the given
 //string as a message.
 func NewNotFoundError(message string) *AppError {
 	return &AppError{http.StatusNotFound, message}
 }
 
-// Creates an error object with HTTP status code 500 and using the given
+// NewUnexpectedError Creates an error object with HTTP status code 500, using the given
 //string as a message.
 func NewUnexpectedError(message string) *AppError {
 	return &AppError{http.StatusInternalServerError, message}
 }
 
-// Creates an error object with HTTP status code 422 and using the given
+// NewValidationError Creates an error object with HTTP status code 422, using the given
 //string as a message.
 func NewValidationError(message string) *AppError {
 	return &AppError{http.StatusUnprocessableEntity, message}
 }
 
-// Creates an error object with HTTP status code 400 and using the given
+// NewBadRequestError Creates an error object with HTTP status code 400, using the given
 //string as a message.
 func NewBadRequestError(message string) *AppError {
 	return &AppError{http.StatusBadRequest, message}
 }
 
-// NewUnauthorizedError creates an error object with HTTP status code 403 and using the given
+// NewUnauthorizedError creates an error object with HTTP status code 403, using the given
 // string as a message.
 func NewUnauthorizedError(message string) *AppError {
 	return &AppError{http.StatusForbidden, message}
